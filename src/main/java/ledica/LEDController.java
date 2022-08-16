b3227101470b88a5112c14948acc64ceb054cf3a
ggity <ggiiggity@gmail.com>
2022-08-16 03:04:39 +0200

    ponovo izmjene

--- src/main/java/ledica/LEDController.java
+++ src/main/java/ledica/LEDController.java
@@ -1,6 +1,7 @@
 package ledica;
 
 import com.pi4j.*;
+import com.pi4j.io.gpio.Gpio;
 import com.pi4j.io.gpio.digital.DigitalOutput;
 //import com.pi4j.context
 import com.pi4j.io.gpio.digital.DigitalState;
@@ -9,22 +10,30 @@ public class LEDController {
 
 	public static void main(String[] args) throws Exception {
 		System.out.println("zdravo");
-		
+
 		System.out.println(LEDController.class.getCanonicalName());
-		
+
 		var pi4j = Pi4J.newAutoContext();
-		
-		var ledConfig = DigitalOutput.newConfigBuilder(pi4j)
-				.id("id")
-				.name("ime")
-				.address(1)
-				.shutdown(DigitalState.LOW)
+
+		var ledConfig = DigitalOutput.newConfigBuilder(pi4j).id("id").name("ime").address(1).shutdown(DigitalState.LOW)
 				.initial(DigitalState.HIGH);
-				
+//				.build();
+
+//		System.out.println(ledConfig);
+		var ledica = pi4j.dout().create(ledConfig);
+//		System.out.println(ledica);
+		System.out.println("palim");
+		ledica.state(DigitalState.HIGH);
+
+//		GpioPinD
+//		DigitalOutput.
+//		ledConfig.	
+//		ledConfig.state
 		Thread.sleep(5000);
-		
 		System.out.println("gasim");
-		
+		ledica.toggle();
+		Thread.sleep(2000);
+
 		pi4j.shutdown();
 	}
 
