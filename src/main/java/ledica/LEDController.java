2eab4da5e70d33c54183da48c96ccc5973cfd357
ggity <ggiiggity@gmail.com>
2022-08-16 04:20:33 +0200

    lalalala

--- src/main/java/ledica/LEDController.java
+++ src/main/java/ledica/LEDController.java
@@ -3,9 +3,13 @@ package ledica;
 import com.pi4j.*;
 import com.pi4j.io.gpio.Gpio;
 import com.pi4j.io.gpio.digital.DigitalOutput;
+import com.pi4j.io.gpio.digital.DigitalOutputProvider;
 //import com.pi4j.context
 import com.pi4j.io.gpio.digital.DigitalState;
+import com.pi4j.library.pigpio.PiGpio;
+import com.pi4j.plugin.pigpio.provider.gpio.digital.PiGpioDigitalOutput;
 import com.pi4j.provider.Provider;
+import com.pi4j.provider.Providers;
 import com.pi4j.util.Console;
 
 public class LEDController {
@@ -19,17 +23,19 @@ public class LEDController {
 		
 		var pi4j = Pi4J.newAutoContext();
 		Console konzola = new Console();
+		System.out.println(pi4j.getDigitalOutputProvider().description()	);
 		
 		System.out.println(pi4j.providers());
 		
 		var ledConfig = DigitalOutput.newConfigBuilder(pi4j)
 				.id("id")
 				.name("ime")
-				.address(18)
+				.address(12)
 				.shutdown(DigitalState.LOW)
 				.initial(DigitalState.HIGH);	
 //				.build();
 				
+//		PiGpioDigitalOutput ledara = new PiGpioDigitalOutput(18, , "faf");
 		konzola.println("blebe");
 //		System.out.println(ledConfig);
 		var ledica = pi4j.dout().create(ledConfig);
@@ -41,11 +47,11 @@ public class LEDController {
 //		System.out.println(ledica);
 		System.out.println("palim");
 		ledica.state(DigitalState.HIGH);
-		
+//		PiGpioDigitalOutput ledara = new PiGpioDigitalOutput(18, , null)
 		System.out.println();
-		
+//		PiGpioDigitalOutput ledara = new PiGpioDigitalOutput(18, ledica.provider(), ledConfig);
 //		GpioFa
-		
+//		PiGpioDigitalOutput d = new 
 			
 		
 //		GpioPinD
@@ -57,6 +63,8 @@ public class LEDController {
 		ledica.toggle();
 		konzola.println(ledica.state());	
 		konzola.println(ledica.provider());
+//		PiGpioDigitalOutput dout 
+//		DigitalOutput at = new PiGpioDigitalOutput( , null, null)
 		Thread.sleep(2000);
 		
 		
