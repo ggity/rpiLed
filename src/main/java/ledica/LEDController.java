db89fb5cfa960d4ef6a91067c11334af168029cf
ggity <ggiiggity@gmail.com>
2022-08-16 18:26:16 +0200

    mozda proradi

--- src/main/java/ledica/LEDController.java
+++ src/main/java/ledica/LEDController.java
@@ -1,12 +1,14 @@
 package ledica;
 
 import com.pi4j.*;
+import com.pi4j.context.Context;
 import com.pi4j.io.gpio.Gpio;
 import com.pi4j.io.gpio.digital.DigitalOutput;
 import com.pi4j.io.gpio.digital.DigitalOutputProvider;
 //import com.pi4j.context
 import com.pi4j.io.gpio.digital.DigitalState;
 import com.pi4j.library.pigpio.PiGpio;
+import com.pi4j.platform.Platforms;
 import com.pi4j.plugin.pigpio.provider.gpio.digital.PiGpioDigitalOutput;
 import com.pi4j.provider.Provider;
 import com.pi4j.provider.Providers;
@@ -15,65 +17,33 @@ import com.pi4j.util.Console;
 public class LEDController {
 
 	public static void main(String[] args) throws Exception {
-		System.out.println("zdravo");
 		
-		
-		
-		System.out.println(LEDController.class.getCanonicalName());
-		
-		var pi4j = Pi4J.newAutoContext();
 		Console konzola = new Console();
-		System.out.println(pi4j.getDigitalOutputProvider().description()	);
 		
-		System.out.println(pi4j.providers());
+		var pi = Pi4J.newAutoContext();
 		
-		var ledConfig = DigitalOutput.newConfigBuilder(pi4j)
-				.id("id")
-				.name("ime")
-				.address(12)
-				.shutdown(DigitalState.LOW)
-				.initial(DigitalState.HIGH)	
-				.build();
-				
-//		PiGpioDigitalOutput ledara = new PiGpioDigitalOutput(18, , "faf");
-		konzola.println("blebe");
-//		System.out.println(ledConfig);
-//		var ledica = pi4j.dout().create(ledConfig);
-//		konzola.println(ledica.state());
-//		System.out.println(ledica.provider());
-//		System.out.println(ledica.getAddress());
-//		System.out.println(ledica.getName());
-//		System.out.println(ledica.get
-//		System.out.println(ledica);
-		System.out.println("palim");
-//		ledica.state(DigitalState.HIGH);
-//		PiGpioDigitalOutput ledara = new PiGpioDigitalOutput(18, , null)
-		System.out.println();
-//		PiGpioDigitalOutput ledara = new PiGpioDigitalOutput(18, ledica.provider(), ledConfig);
-//		GpioFa
-//		PiGpioDigitalOutput d = new 
+		Platforms platforma = pi.platforms();
+		
+		konzola.box("Pi platforma:");
+		platforma.describe().print(System.out);
+		
+		var ledConfig = DigitalOutput.newConfigBuilder(pi)
+			      .id("led")
+			      .name("LED Flasher")
+			      .address(1)
+			      .shutdown(DigitalState.LOW)
+			      .initial(DigitalState.LOW)
+			      .provider("pigpio-digital-output");
+			      
+			var led = pi.create(ledConfig);
 			
-		System.out.println(pi4j.platform().description());//		GpioPinD
-		var izlaz = pi4j.platform().digitalOutput().create(ledConfig);
-		System.out.println(izlaz.state());
-		izlaz.state(DigitalState.HIGH);
-//		izlaz.state(DigitalState.HIGH);
-		System.out.println(pi4j.platform().digitalOutput().create(12).description());
-//		DigitalOutput.
-//		ledConfig.	
-//		ledConfig.state
-		Thread.sleep(5000);
-		System.out.println("gasim");
-//		ledica.toggle();
-//		konzola.println(ledica.state());	
-//		konzola.println(ledica.provider());
-//		PiGpioDigitalOutput dout 
-//		DigitalOutput at = new PiGpioDigitalOutput( , null, null)
-		Thread.sleep(2000);
+		for (int i = 1; i <= 5; i++) {
+			led.toggle();
+			Thread.sleep(3000);
+		}
 		
+		pi.shutdown();
 		
-		
-		pi4j.shutdown();
 	}
 
 }
